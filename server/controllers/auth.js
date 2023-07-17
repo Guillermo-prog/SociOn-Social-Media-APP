bc912386662902f5614ff784b3707c5964d89ba5
Guillermo-prog <72439217+Guillermo-prog@users.noreply.github.com>
2023-07-18 01:02:39 +0200

    feat(server): implement User register functionality

---
+++ server/controllers/auth.js
@@ -0,0 +1,39 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+// REGISTER USER FUNCTION
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body; //extracted from FrontEnd
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = bcrypt.salt(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      password: passwordHash,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 //Configuraciones MiddleWare
 const __filename = fileURLToPath(import.meta.url);
@@ -51,6 +52,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+//ROUTES
+app.post("/auth/register", upload.single("picture"), register); //(route, middelware, logicAction=controller)
 
 //MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
