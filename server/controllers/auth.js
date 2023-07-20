f2b379cb25430f7ed948daf564d969c68724b03c
Guillermo-prog <72439217+Guillermo-prog@users.noreply.github.com>
2023-07-20 11:02:30 +0200

    fix(server): user register corrections

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -17,14 +17,14 @@ export const register = async (req, res) => {
     } = req.body; //extracted from FrontEnd
 
     const salt = await bcrypt.genSalt();
-    const passwordHash = bcrypt.salt(password, salt);
+    const passwordHash = await bcrypt.hash(password, salt);
 
     const newUser = new User({
       firstName,
       lastName,
       email,
-      password,
       password: passwordHash,
+      picturePath,
       friends,
       location,
       occupation,
