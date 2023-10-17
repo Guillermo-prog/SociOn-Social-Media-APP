35fa1f7a3e049e07d15d552758f3e9e0adab7c16
Guillermo-prog <72439217+Guillermo-prog@users.noreply.github.com>
2023-10-17 11:14:03 +0200

    feat(server): implement User basic routes

--- server/index.js
+++ server/index.js
@@ -10,11 +10,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 
 //Configuraciones MiddleWare
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); // toPath -> /ruta/a/archivo.js, import.meta.url -> file:///ruta/a/archivo.js
+const __dirname = path.dirname(__filename); // directorio del archivo Ej.: /ruta/a
 
 //Enviroment variables in a file .env
 dotenv.config();
@@ -27,14 +29,16 @@ app.use(express.json());
 
 //Hide HTTP responses confidential headers
 app.use(helmet());
+
 //cross-domain access to resources
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
-//HTTP request logger (common format in console)
+//HTTP request logger (common format of requests in console)
 app.use(morgan("common"));
 
 //Data from JSONS URL format to JS readable
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
+
 //Data from forms in URL format to JS readable
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
@@ -55,12 +59,15 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-//ROUTES with FILES Management (Need to use upload middelware)
+//ROUTES with FILES Management (Need to use upload middelware, so we mantain them in Index.js)
+//upload.single("picture") => to grab the picture property of the HTTP req where the image is located and to upload it
 app.post("/auth/register", upload.single("picture"), register); //(route, middelware, logicAction=controller)
+app.post("/posts", upload.single("picture"), createPost);
 
 //ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 //MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
