30ed5c0f74868b81d03c8bebc7133df355c82ab8
Guillermo-prog <72439217+Guillermo-prog@users.noreply.github.com>
2023-11-29 23:36:06 +0100

    fix(client): corrections and disabled friend button for logged user posts

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -1,17 +1,18 @@
 import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
-import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Box, IconButton, Button, Typography, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "state";
 import FlexBetween from "./FlexBetween";
 import UserImage from "./UserImage";
 import { useNavigate } from "react-router-dom";
 
-const Friend = ({ friendId, name, subtitle, usePicturePath }) => {
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { _id } = useSelector((state) => state.user);
   const token = useSelector((state) => state.token);
   const friends = useSelector((state) => state.user.friends);
+  const user = useSelector((state) => state.user);
 
   const { palette } = useTheme();
   const primaryLight = palette.primary.light;
@@ -19,9 +20,11 @@ const Friend = ({ friendId, name, subtitle, usePicturePath }) => {
   const main = palette.neutral.dark;
   const medium = palette.neutral.dark;
 
-  const isFriend = friends.find(
+  const isFriend = Array.from(friends).find(
     (friend) => friend._id === friendId || friend === friendId
   );
+
+  const isLoggedUser = user._id === friendId;
 
   const patchFriend = async () => {
     const response = await fetch(
@@ -42,7 +45,7 @@ const Friend = ({ friendId, name, subtitle, usePicturePath }) => {
     <FlexBetween>
       <FlexBetween gap="1rem">
         <UserImage
-          image={usePicturePath}
+          image={userPicturePath}
           size="55px"
           sx={{
             "&hover": { color: palette.primary.light, cursor: "pointer" },
@@ -69,16 +72,34 @@ const Friend = ({ friendId, name, subtitle, usePicturePath }) => {
           </Typography>
         </Box>
       </FlexBetween>
-      <IconButton
-        onClick={() => patchFriend()}
-        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
-      >
-        {isFriend ? (
-          <PersonRemoveOutlined sx={{ color: primaryDark }} />
-        ) : (
-          <PersonAddOutlined sx={{ color: primaryDark }} />
-        )}
-      </IconButton>
+      {!isLoggedUser ? (
+        <IconButton
+          onClick={() => patchFriend()}
+          sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+        >
+          {isFriend ? (
+            <PersonRemoveOutlined sx={{ color: primaryDark }} />
+          ) : (
+            <PersonAddOutlined sx={{ color: primaryDark }} />
+          )}
+        </IconButton>
+      ) : (
+        <IconButton
+          disabled
+          sx={{
+            "&:disabled": {
+              backgroundColor: primaryLight,
+            },
+            p: "0.6rem",
+            color: palette.background.alt,
+            borderRadius: "3rem",
+          }}
+        >
+          <Typography variant="h6" color={primaryDark}>
+            Yours
+          </Typography>
+        </IconButton>
+      )}
     </FlexBetween>
   );
 };
